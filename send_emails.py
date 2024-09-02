c37d8ea827affde3f6360966a7ad8a66889e231d
itlubber <39397838+itlubber@users.noreply.github.com>
2024-09-03 00:28:26 +0800

    Update send_emails.py

--- send_emails.py
+++ send_emails.py
@@ -19,12 +19,12 @@ def send_email():
     item = Message(
         account=account,
         folder=account.sent,
-        subject=f'即科金融-商户收单变量表-{today}',
+        subject=f'XXXX-商户收单变量表-{today}',
         body=HTMLBody(f"邮件主题: 商户监控相关变量<br/>更新时间: {today}<br/>附件内容: 商户收单变量数据字典、商户衍生变量数据字典、商户变量表"),
         to_recipients=[Mailbox(email_address=e) for e in email_options["receivers"]],
     )
 
-    item.attach(FileAttachment(name=f'即科金融-商户收单变量表-{today}.xlsx', content=open('商户相关变量.xlsx', 'rb').read()))
+    item.attach(FileAttachment(name=f'XXXX-商户收单变量表-{today}.xlsx', content=open('商户相关变量.xlsx', 'rb').read()))
 
     item.send()
 
